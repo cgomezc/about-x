b93a288e0661f5cbe9c38a057c793b93b926ac8c
cruz_cardenas <cardenasg.cruz@gmail.com>
2017-08-21 09:02:14 -0500

    Se agrega texto de tituloH1 y descripcion

--- js/aboutAPI.js
+++ js/aboutAPI.js
@@ -1,3 +1,4 @@
+// url de la API
 var urlAPI = 'https://experienciasxcaret.github.io/Developers/api/front.json';
 new Vue({
 el: '#wrapper',
@@ -5,14 +6,17 @@ created: function () {
   this.consultarAPI();
 },
 data: {
-  atractivos : []
+  atractivos : [],
+  contenido : []
 },
 methods:{
     consultarAPI:function () {
+      // Obtiene los datos para mostrar
       this.$http.get(urlAPI)
           .then(function(respuesta){
-
+          // La respuesta se guarda en el modelo
           this.atractivos = respuesta.body[0].atractivos;
+          this.contenido = respuesta.body[0];
           });
     }
 }
